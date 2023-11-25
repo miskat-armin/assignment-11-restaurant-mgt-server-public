8fb7ea14b3e09227e2f5958a8ca17f66f509f4aa
ZahidHasan321 <dev.zahidhasan@gmail.com>
2023-11-26 00:42:25 +0600

    initial commit

---
+++ index.mjs
@@ -0,0 +1,27 @@
+import cors from "cors";
+import express from "express";
+import "express-async-errors";
+import "./loadEnvironment.mjs";
+import brands from './routes/brands.mjs'
+import carts from './routes/carts.mjs'
+import products from './routes/products.mjs'
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/brands', brands);
+app.use('/api/products', products)
+app.use('/api/carts', carts)
+
+// Global error handling
+app.use((err, _req, res, next) => {
+  res.status(500).send("Uh oh! An unexpected error occured.")
+})
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
